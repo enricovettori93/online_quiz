fbbf80ed7c90c76d5a9020aa2723568126b58467
Enrico Vettori <e.vettori@egeatech.com>
2019-07-10 19:00:56 +0200

    WIP AddQuestion form layout and bindings

--- client/src/components/addquestion/addquestion.js
+++ client/src/components/addquestion/addquestion.js
@@ -4,7 +4,25 @@ export default {
   props: [],
   data () {
     return {
-
+      formLabel: {
+        question: 'Inserisci la domanda',
+        answer1: 'Inserisci la prima risposta',
+        answer2: 'Inserisci la seconda risposta',
+        answer3: 'Inserisci la terza risposta',
+        answer4: 'Inserisci la quarta risposta',
+        correct: 'Inserisci la risposta esatta',
+        file: 'Inserisci l\'immagine',
+        description: 'Inserisci la descrizione',
+        go: 'Aggiungi la domanda'
+      },
+      question: null,
+      answer1: null,
+      answer2: null,
+      answer3: null,
+      answer4: null,
+      correct: null,
+      file: null,
+      description: null,
     }
   },
   computed: {
@@ -14,6 +32,12 @@ export default {
 
   },
   methods: {
-
+    goForm() {
+      // eslint-disable-next-line no-console
+      console.log('subit del form');
+    },
+    filesChange(files) {
+      this.file = files[0] || null;
+    }
   }
 }
